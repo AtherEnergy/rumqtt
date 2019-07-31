68bbe5cef653b0f17e9ce09d6cdeef555518363a
raviteja <ravi.teja@atherenergy.com>
2019-07-31 11:19:10 +0530

    Fix: crash during second continous disconnection

--- src/client/connection.rs
+++ src/client/connection.rs
@@ -256,8 +256,8 @@ impl Connection {
 
     /// Sends connection status on blocked connections status call in `run`
     fn handle_connection_success(&mut self) {
-        if self.connection_count == 0 {
-            let connection_tx = self.connection_tx.take().unwrap();
+        // send connection success notification only the first time
+        if let Some(connection_tx) = self.connection_tx.take() {
             connection_tx.try_send(Ok(())).unwrap();
         } else {
             let _ = self.notification_tx.try_send(Notification::Reconnection);
@@ -274,8 +274,8 @@ impl Connection {
             None => Err(ConnectError::Timeout),
         };
 
-        if self.connection_count == 0 {
-            let connection_tx = self.connection_tx.take().unwrap();
+        // send connection error notification only the first time
+        if let Some(connection_tx) = self.connection_tx.take() {
             connection_tx.try_send(error).unwrap();
         }
     }
